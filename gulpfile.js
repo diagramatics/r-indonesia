1ac508932083748fd93a661812905292692c7d30
Steven Sinatra <ohai@diagramatics.me>
2015-01-04 06:50:51 +1100

    Working (buggish) prototype of Gulp live reload!
    Finally!

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 // Note: edit this according to the Reddit subreddit to test
 var subreddit = "indoclone"
 var bsPort = 3500;
+// --------
 
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
@@ -8,15 +9,11 @@ var $ = require('gulp-load-plugins')();
 // for the stylesheet
 var browserSync = require('browser-sync');
 var util = require('util');
-
-gulp.task('sass', function() {
-
-});
+var runSequence = require('run-sequence');
 
 gulp.task('reddit-proxy', function() {
   browserSync({
     proxy: "http://www.reddit.com/r/" + subreddit,
-    files: "style.css",
     port: bsPort + 1,
 
     // Use the snippetOptions to find the current subreddit CSS if any
@@ -27,7 +24,7 @@ gulp.task('reddit-proxy', function() {
         // If there's none, just match it with the </head>
         match: new RegExp('<link rel="stylesheet" href="[^"]*" title="applied_subreddit_stylesheet" type="text/css">|</head>', 'i'),
         fn: function(snippet, match) {
-          return snippet + '<link rel="stylesheet" href="http://localhost:'+bsPort+'/style.css" title="applied_subreddit_stylesheet" type="text/css">' + (match === '</head>' ? match : '');
+          return snippet + '<link rel="stylesheet" href="http://localhost:'+bsPort+'/css/style.css" title="applied_subreddit_stylesheet" type="text/css">' + (match === '</head>' ? match : '');
         }
       }
     }
@@ -58,4 +55,52 @@ gulp.task('setup-servers', function() {
   })
 });
 
-gulp.task('dev', ['setup-servers']);
+gulp.task('styles', function() {
+  return gulp.src('scss/style.scss')
+    .pipe($.sass({
+      outputStyle: 'nested',
+      precision: 3,
+      onError: console.error.bind(console, 'Sass error:')
+    }))
+    // TODO: For some reason not working properly. Use $.autoprefixer fallback
+    // .pipe($.postcss([
+    //   require('autoprefixer-core')({browsers: ['last 2 version']})
+    // ]))
+    .pipe($.autoprefixer({browsers: 'last 2 version'}))
+    .pipe(gulp.dest('css/'));
+});
+
+gulp.task('post-style-dev', function() {
+  var fs = require('fs');
+  var postcss = require('postcss');
+  var url = require('postcss-url');
+  var result = postcss()
+    .use(url({
+      url: function(url) {
+        var name = url.substring(2, url.length - 2);
+        if (fs.existsSync('images/' + name + '.jpg')) {
+          return '"http://localhost:' + bsPort + '/images/' + name + '.jpg"';
+        }
+        else return '"http://localhost:' + bsPort + '/images/' + name + '.png"';
+      }
+    }))
+    .process(fs.readFileSync('css/style.css'))
+    .css;
+  fs.writeFile('css/style.css', result);
+  return gulp.src('css/style.css').pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('style-dev', function() {
+  runSequence('styles', 'post-style-dev');
+});
+
+gulp.task('build', function() {
+  gulp.src('css/style.css')
+    .pipe($.csso())
+    .pipe(gulp.dest('dist/css/'));
+});
+
+
+gulp.task('dev', ['setup-servers', 'style-dev'], function() {
+  gulp.watch('scss/**/*.scss', ['style-dev'])
+});
