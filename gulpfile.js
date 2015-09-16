fe5716ce22a32634f08fb52d991434d3d8ec269b
Steven Sinatra <ohai@diagramatics.me>
2015-09-16 12:21:02 +1000

    Implement script checking on test script

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var util = require('util');
 var fs = require('fs');
 var postcss = require('postcss');
 var browserSync = require('browser-sync');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 // Configurations
 var config = {
@@ -44,40 +45,40 @@ gulp.task('setup-servers', function() {
 
 gulp.task('sass', function () {
   return gulp.src(config.style)
-    .pipe($.sourcemaps.init())
-    .pipe($.sass({
-      outputStyle: 'expanded',
-      precision: 10,
-      includePaths: ['.']
-    }).on('error', $.sass.logError))
-    .pipe($.postcss([
-      require('autoprefixer-core')({browsers: ['last 1 version', 'ie 8']})
-    ]))
-    .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('.tmp/css'));
+  .pipe($.sourcemaps.init())
+  .pipe($.sass({
+    outputStyle: 'expanded',
+    precision: 10,
+    includePaths: ['.']
+  }).on('error', $.sass.logError))
+  .pipe($.postcss([
+    require('autoprefixer-core')({browsers: ['last 1 version', 'ie 8']})
+  ]))
+  .pipe($.sourcemaps.write())
+  .pipe(gulp.dest('.tmp/css'));
 });
 
 gulp.task('styles:dev', ['sass'], function() {
   fs.writeFile(config.tmpStyle, postcss()
-    .use(require('postcss-url')({
-      url: function(url) {
-        // Check if the URL is using the Reddit image URL format
-        if (url.match(/^%%.+%%{0}/i)) {
-          var name = url.substring(2, url.length - 2);
-          if (fs.existsSync('images/' + name + '.jpg')) {
-            return '"//localhost:' + bsPort + '/images/' + name + '.jpg"';
-          }
-          else if (fs.existsSync('images/' + name + '.png')) {
-            return '"//localhost:' + bsPort + '/images/' + name + '.png"';
-          }
+  .use(require('postcss-url')({
+    url: function(url) {
+      // Check if the URL is using the Reddit image URL format
+      if (url.match(/^%%.+%%{0}/i)) {
+        var name = url.substring(2, url.length - 2);
+        if (fs.existsSync('images/' + name + '.jpg')) {
+          return '"//localhost:' + bsPort + '/images/' + name + '.jpg"';
+        }
+        else if (fs.existsSync('images/' + name + '.png')) {
+          return '"//localhost:' + bsPort + '/images/' + name + '.png"';
         }
       }
-    }))
-    .process(fs.readFileSync(config.tmpStyle))
-    .css
-  );
-  return gulp.src(config.tmpStyle)
-    .pipe(browserSync.stream());
+    }
+  }))
+  .process(fs.readFileSync(config.tmpStyle))
+  .css
+);
+return gulp.src(config.tmpStyle)
+.pipe(browserSync.stream());
 });
 
 gulp.task('parse', function() {
@@ -98,49 +99,49 @@ gulp.task('parse', function() {
 
 gulp.task('process-svg', function(cb) {
   gulp.src('images/sprites/*.svg')
-    .pipe($.svgSprite({
-      // Configuration is to generate an SCSS file in the appropriate directory
-      // and the sprites in the images for PNG conversion
-      mode: {
-        css: {
-          dest: '.',
-          sprite: './images/icon-sprites.png', // Use .png immediately since we're working on converting it to PNG anyway
-          bust: false, // No need for cache busting
-          render: {
-            scss: {
-              template: 'build/svg-sprite-template.scss',
-              dest: 'scss/sprites/_sprites.scss'
-            }
-          },
-          mixin: 'icon-sprite',
-          dimensions: true
+  .pipe($.svgSprite({
+    // Configuration is to generate an SCSS file in the appropriate directory
+    // and the sprites in the images for PNG conversion
+    mode: {
+      css: {
+        dest: '.',
+        sprite: './images/icon-sprites.png', // Use .png immediately since we're working on converting it to PNG anyway
+        bust: false, // No need for cache busting
+        render: {
+          scss: {
+            template: 'build/svg-sprite-template.scss',
+            dest: 'scss/sprites/_sprites.scss'
+          }
+        },
+        mixin: 'icon-sprite',
+        dimensions: true
+      }
+    }
+  }))
+  .pipe(gulp.dest('.'))
+  .pipe($.filter('**/*.png'))
+  .pipe($.svg2png())
+  .pipe(gulp.dest('.'))
+  .on('end', function() {
+    fs.writeFile('scss/sprites/_sprites.scss', postcss()
+    // Change the URL format to Reddit's image URL format.
+    .use(require('postcss-url')({
+      url: function(url) {
+        if (url.match(/^images\//i)) {
+          var name = url.substring('images/'.length, url.length - '.png'.length);
+          return '%%' + name + '%%';
         }
+
+        return url;
       }
     }))
-    .pipe(gulp.dest('.'))
-    .pipe($.filter('**/*.png'))
-    .pipe($.svg2png())
-    .pipe(gulp.dest('.'))
-    .on('end', function() {
-      fs.writeFile('scss/sprites/_sprites.scss', postcss()
-        // Change the URL format to Reddit's image URL format.
-        .use(require('postcss-url')({
-          url: function(url) {
-            if (url.match(/^images\//i)) {
-              var name = url.substring('images/'.length, url.length - '.png'.length);
-              return '%%' + name + '%%';
-            }
+    .process(fs.readFileSync('scss/sprites/_sprites.scss'))
+    .css
+  );
 
-            return url;
-          }
-        }))
-        .process(fs.readFileSync('scss/sprites/_sprites.scss'))
-        .css
-      );
-
-      browserSync.reload();
-      cb();
-    });
+  browserSync.reload();
+  cb();
+});
 });
 
 gulp.task('styles:build', ['sass'], function() {
@@ -172,17 +173,54 @@ gulp.task('styles:build', ['sass'], function() {
 
   // Now minify and send it to the dist folder
   return gulp.src(config.tmpStyle)
-    .pipe($.csso())
-    .pipe(gulp.dest('dist/css/'));
+  .pipe($.csso())
+  .pipe(gulp.dest('dist/css/'));
 });
 
 gulp.task('test-script', function() {
-  return gulp.src('test-script/*.js')
+  var versionJSON = require('./test-script/version.json');
+
+  gulp.src('test-script/version.json')
     .pipe(gulp.dest('dist/test-script'));
+    
+  return gulp.src('test-script/test-script.user.js')
+  .pipe($.insert.transform(function(contents, file) {
+    return contents.replace(/\{\{version\}\}/g, versionJSON.version);
+  }))
+  .pipe(gulp.dest('dist/test-script'));
+});
+
+gulp.task('images', function() {
+  return gulp.src('images/*.{png,jpg}')
+  .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('build', ['styles:build', 'test-script'], function() {
+
   return gulp.src(config.distStyle).pipe($.size());
+});
+
+gulp.task('build:open-beta', ['build', 'images'], function() {
+  fs.writeFile(config.distStyle, postcss()
+  .use(require('postcss-url')({
+    url: function(url) {
+      // Check if the URL is using the Reddit image URL format
+      if (url.match(/^%%.+%%{0}/i)) {
+        var name = url.substring(2, url.length - 2);
+        if (fs.existsSync('images/' + name + '.jpg')) {
+          return '"../images/' + name + '.jpg"';
+        }
+        else if (fs.existsSync('images/' + name + '.png')) {
+          return '"../images/' + name + '.png"';
+        }
+      }
+    }
+  }))
+  .process(fs.readFileSync(config.distStyle))
+  .css
+);
+
+return gulp.src(config.distStyle).pipe($.size());
 });
 
 gulp.task('prebuild:beta-test', function() {
@@ -192,20 +230,20 @@ gulp.task('prebuild:beta-test', function() {
 });
 gulp.task('build:beta-test', ['prebuild:beta-test', 'build'], function() {
   fs.writeFile(config.distStyle, postcss()
-    .use(function(css) {
-      css.walkRules(function(rule) {
-        // TODO: Put this on a separate config file
-        // Because we're using html selector and body selectors, merge together if there's a child element with the same name in the selector
-        rule.selector = rule.selector.replace(/^body\.post-linkflair-beta:lang\(bt\) body/ig, 'body.post-linkflair-beta:lang(bt)');
-        rule.selector = rule.selector.replace(/^html:not\(:lang\(bt\)\) html/ig, 'html:not(:lang(bt))');
-        // Also use the beta linkflair selector and merge it with the longpost for testing
-        rule.selector = rule.selector.replace(/ \.comments-page/ig, '.comments-page');
-        rule.selector = rule.selector.replace(/\.post-linkflair-longpost/ig, '.post-linkflair-beta');
-      });
-    })
-    .process(fs.readFileSync(config.distStyle))
-    .css
-  );
+  .use(function(css) {
+    css.walkRules(function(rule) {
+      // TODO: Put this on a separate config file
+      // Because we're using html selector and body selectors, merge together if there's a child element with the same name in the selector
+      rule.selector = rule.selector.replace(/^body\.post-linkflair-beta:lang\(bt\) body/ig, 'body.post-linkflair-beta:lang(bt)');
+      rule.selector = rule.selector.replace(/^html:not\(:lang\(bt\)\) html/ig, 'html:not(:lang(bt))');
+      // Also use the beta linkflair selector and merge it with the longpost for testing
+      rule.selector = rule.selector.replace(/ \.comments-page/ig, '.comments-page');
+      rule.selector = rule.selector.replace(/\.post-linkflair-longpost/ig, '.post-linkflair-beta');
+    });
+  })
+  .process(fs.readFileSync(config.distStyle))
+  .css
+);
 });
 
 
--- test-script/test-script.user.js
+++ test-script/test-script.user.js
@@ -5,20 +5,84 @@
 // @namespace   http://diagramatics.me
 // @source      http://github.com/diagramatics/r-indonesia
 // @include     https://bt.reddit.com/r/indonesia/*
-// @version     1.0.0
-// @grant       none
+// @version     {{version}}
+// @grant       GM_getValue
+// @grant       GM_setValue
 // ==/UserScript==
 
 (function() {
-  var injectedStyle = '<link rel="stylesheet" href="https://cdn.rawgit.com/diagramatics/r-indonesia/dist/css/style.css" title="applied_subreddit_stylesheet" type="text/css">';
-  // Find the custom subreddit styling
-  var style = document.querySelector('link[title="applied_subreddit_stylesheet"]');
-  if (style !== null) {
-    style.insertAdjacentHTML('afterend', injectedStyle);
-    style.remove();
+  var version = '{{version}}';
+
+  var checkScriptVersion = function(sha) {
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://cdn.rawgit.com/diagramatics/r-indonesia/'+ sha +'/test-script/version.json');
+
+    request.onload = function() {
+      var response = JSON.parse(this.response);
+      if (response.version !== version) {
+        window.prompt('You\'re using an older version of the script. Please redownload it again at the link below.', 'https://cdn.rawgit.com/diagramatics/r-indonesia/'+ sha +'/test-script/test-script.user.js');
+      }
+    };
+
+    request.onerror = function() {
+      // There was a connection error of some sort
+      console.error('Seems like we cannot check the recent version of the script. We\'ll do it later.');
+    };
+
+    request.send();
   }
-  else {
-    var head = document.querySelector('head');
-    head.insertAdjacentHTML('beforeend', injectedStyle);
+
+  var checkSha = function(cb) {
+    var lastCheckDate = new Date(GM_getValue('lastCheckDate', null));
+    var lastSha = GM_getValue('lastSha', null);
+    // Check if last SHA check date was yesterday
+    if (!lastSha || !lastCheckDate || lastCheckDate.setDate(lastCheckDate.getDate() + 1) < new Date()) {
+      var request = new XMLHttpRequest();
+      request.open('GET', 'https://api.github.com/repos/diagramatics/r-indonesia/git/refs/heads/dist', true);
+
+      request.onload = function() {
+        var response = JSON.parse(this.response);
+        GM_setValue('lastCheckDate', new Date());
+        GM_setValue('lastSha', response.object.sha);
+        cb(response.object.sha);
+      };
+
+      request.onerror = function() {
+        // There was a connection error of some sort
+        console.error('Seems like we cannot check the recent version of the stylesheet. Reverting to saved SHA if there is any.');
+        if (lastSha) {
+          cb(lastSha);
+        }
+      };
+
+      request.send();
+    }
+    else {
+      if (lastSha) {
+        cb(lastSha);
+      }
+      else {
+        console.log('We don\'t have the latest version of stylesheet. Weird. Contact diagramatics for this.');
+      }
+    }
   }
+
+  var injectStyle = function(sha) {
+    var injectedStyle = '<link rel="stylesheet" href="https://cdn.rawgit.com/diagramatics/r-indonesia/'+ sha +'/css/style.css" title="applied_subreddit_stylesheet" type="text/css">';
+    // Find the custom subreddit styling
+    var style = document.querySelector('link[title="applied_subreddit_stylesheet"]');
+    if (style !== null) {
+      style.insertAdjacentHTML('afterend', injectedStyle);
+      style.remove();
+    }
+    else {
+      var head = document.querySelector('head');
+      head.insertAdjacentHTML('beforeend', injectedStyle);
+    }
+  }
+
+  checkSha(function(sha) {
+    injectStyle(sha);
+    checkScriptVersion(sha);
+  });
 }());
