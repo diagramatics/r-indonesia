e60b6f6960d91fbd2686b0c46816502a46dad686
Steven Sinatra <ohai@diagramatics.me>
2015-09-16 19:29:01 +1000

    v1.0.2 of test-script

--- test-script/test-script.user.js
+++ test-script/test-script.user.js
@@ -4,15 +4,18 @@
 // @description A loader for r/indonesia subreddit's new design (v2).
 // @namespace   http://diagramatics.me
 // @source      http://github.com/diagramatics/r-indonesia
+// @include     https://*.reddit.com/r/indoclone
+// @include     https://reddit.com/r/indoclone
+// @include     https://bt.reddit.com/r/indonesia
 // @include     https://bt.reddit.com/r/indonesia/
 // @include     https://bt.reddit.com/r/indonesia/*
-// @version     1.0.1
+// @version     1.0.2
 // @grant       GM_getValue
 // @grant       GM_setValue
 // ==/UserScript==
 
 (function() {
-  var version = '1.0.1';
+  var version = '1.0.2';
 
   var checkScriptVersion = function(sha) {
     var request = new XMLHttpRequest();
