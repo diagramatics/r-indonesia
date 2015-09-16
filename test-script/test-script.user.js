24d27b7b927217f028c7dec1501aa68f7c2b19da
Steven Sinatra <ohai@diagramatics.me>
2015-09-16 16:54:33 +1000

    Update test script to include subreddit homepage

--- test-script/test-script.user.js
+++ test-script/test-script.user.js
@@ -4,6 +4,7 @@
 // @description A loader for r/indonesia subreddit's new design (v2).
 // @namespace   http://diagramatics.me
 // @source      http://github.com/diagramatics/r-indonesia
+// @include     https://bt.reddit.com/r/indonesia/
 // @include     https://bt.reddit.com/r/indonesia/*
 // @version     {{version}}
 // @grant       GM_getValue
